a805b3371dfa238257561838238062d50394360a
chenxiaoqing <chenxiaoqing@cnbmtech.com>
2026-03-26 11:06:27 +0800

    license: 添加 AGPL 3.0 许可证
    - 添加 LICENSE 文件（GNU Affero General Public License v3.0）
    - 所有核心源文件添加版权声明头（23 个文件）
    - 版权归属：EasyDB Contributors 2024-2026

--- apps/desktop-ui/src/services/api.ts
+++ apps/desktop-ui/src/services/api.ts
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2024-2026 EasyDB Contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
 /**
  * IPC 服务层 - 封装与 Kotlin 内核的通信
  * 首版使用 HTTP 通信（内核提供本地 HTTP 服务），后续可替换为 Tauri IPC
